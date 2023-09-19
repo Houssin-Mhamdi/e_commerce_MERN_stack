aafd497cd8be3c5749e3b16a6fcaa96d85f7f13d
houssinCarnelian <houssin.carnelian@gmail.com>
2023-09-19 14:15:20 +0100

    make reusable erro class

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
-
 require("dotenv").config()
+
+const ApiError = require("./utlis/apiError")
 const dbConnection = require('./config/database')
 const categoryRoute = require("./routes/categoryRoute")
 
@@ -24,13 +25,22 @@ if(process.env.NODE_ENV === 'development'){
 app.use('/api/v1/categories', categoryRoute)
 
 app.all("*",(req, res, next) =>{
-    const err = new Error(`Can't find this route ${req.originalUrl}`)
-    next(err.message)
+   //const err = new Error(`Can't find this route ${req.originalUrl}`)
+    next(new ApiError(`Can't find this route ${req.originalUrl}`,400))
 })
 
 //Global error handler middleware
 app.use((err,req,res,next)=>{
-res.status(400).json({err})
+    
+    err.statusCode = err.statusCode || 500
+    err.status = err.status || 'error'
+
+res.status(err.statusCode).json({
+    status : err.status,
+    error : err,
+    message: err.message,
+    stack : err.stack
+})
 })
 
 
